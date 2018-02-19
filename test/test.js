7fe48bf9460eed1aa3da1336c099ea6ee3ce6553
Muhamad Ilham Arrouf <ilham.arrouf@gmail.com>
2018-02-19 08:59:47 +0700

    first example test

---
+++ test/test.js
@@ -0,0 +1,17 @@
+const supertest = require('supertest');
+
+const server = supertest.agent('http://localhost:4321');
+
+describe('sample unit test', () => {
+  it('should return json with message `Hello World!`', (done) => {
+    server.get('/')
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(200, {
+        message: 'Hello World!'
+      })
+      .end((err, res) => {
+        if (err) throw err;
+        done();
+      });
+  });
+});
